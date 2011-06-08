d3a05c923f63a71a334a0cf821f59164031a3ee6
craig <craig@haquarter.de>
2011-06-09 00:41:00 +0200

    first commit

---
+++ index.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import cgitb; cgitb.enable()
+import random
+
+print "Content-Type: text/html; charset=utf-8\n"
+
+user=[ 'user1', 'user2', 'user3', 'user4', 'user5', 'user6' ]
+
+print """
+        <HTML><HEAD><TITLE>Random</TITLE></HEAD>
+        <BODY vlink="black">
+
+        <br/> <br/> <br/> <br/>
+        <br/> <br/> <br/> <br/>
+        <div align="center">
+        
+                <H1>%s</H1>
+                <A HREF="">reload</A>
+        </div>
+        </BODY>
+        </HTML>
+
+""" % ( user[ random.randint( 0, (len(user)-1) ) ] )
+
